96f80d2b6e2e70c11e9fe36c8c323f7149717bf7
yadragon2007 <102539850+yadragon2007@users.noreply.github.com>
2025-07-30 13:37:15 +0300

    Job-backoffice layout and navugation

--- Job-backoffice/resources/views/layouts/navigation.blade.php
+++ Job-backoffice/resources/views/layouts/navigation.blade.php
@@ -1,21 +1,35 @@
 <nav class="w-[250px] h-screen bg-white border-r border-gray-200">
     <!-- logo  -->
     <div class="flex items-center px-6 border-b border-gray-200 py-4">
-        <a href="{{ route("dashboard") }}" class="text-2xl font-bold flex items-center space-x-2">
+        <a href="{{ route("dashboard.index") }}" class="text-2xl font-bold flex items-center space-x-2">
             <x-application-logo class="h-6 w-auto fill-current text=gray-800" />
             <span class="text-lg font-semibold text-gray-800">shaghalni</span>
         </a>
     </div>
     <!-- navigation -->
-    <ul>
-        <x-nav-link :href="route('dashboard')" :active="request()->rouyeIs('dashboard')">
+    <ul class="flex flex-col px-4 py-6 space-y-2">
+        <x-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard.index')">
             Dashboard
         </x-nav-link>
-        <x-nav-link :href="route('dashboard')" :active="request()->rouyeIs('dashboard')">
+        <x-nav-link :href="route('company.index')" :active="request()->routeIs('company.index')">
             Companies
         </x-nav-link>
-        <x-nav-link :href="route('dashboard')" :active="request()->rouyeIs('dashboard')">
-                
+        <x-nav-link :href="route('job-category.index')" :active="request()->routeIs('job-category.index')">
+            Job Categories
         </x-nav-link>
+        <x-nav-link :href="route('job-vacancy.index')" :active="request()->routeIs('job-vacancy.index')">
+            Job Vacancies
+        </x-nav-link>
+        <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
+            Users
+        </x-nav-link>
+        <hr>
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <!-- Using utilities: -->
+            <button type="submit" class="bg-red-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Logout
+            </button>
+        </form>
     </ul>
 </nav>
--- Job-backoffice/routes/web.php
+++ Job-backoffice/routes/web.php
@@ -1,17 +1,30 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/', [DashboardController::class, "index"])->middleware(['verified'])->name('dashboard.index');
+
+    Route::get('company', [CompanyController::class, 'index'])->name('company.index');
+    Route::get('job-application', [JobApplicationController::class, 'index'])->name('job-application.index');
+    Route::get('job-category', [JobCategoryController::class, 'index'])->name('job-category.index');
+    Route::get('job-vacancy', [JobVacancyController::class, 'index'])->name('job-vacancy.index');
+    Route::get('user', [UserController::class, 'index'])->name('user.index');
+
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
